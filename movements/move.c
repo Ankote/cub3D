d0733f413efb5f2498eb297114254383ac862df6
Ayoub Ankote <aankote@e1r2p1.1337.ma>
2023-05-13 08:30:17 +0100

    UPDATED

--- movements/move.c
+++ movements/move.c
@@ -6,7 +6,7 @@
 /*   By: aankote <aankote@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 15:32:52 by aankote           #+#    #+#             */
-/*   Updated: 2023/05/13 08:28:46 by aankote          ###   ########.fr       */
+/*   Updated: 2023/05/13 08:30:10 by aankote          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,12 @@
     int x;
     int y;
     
+    data->player.walk_dir = 1; 
     data->player.movestep = data->player.walk_dir * MOVE_SPEED;
 	x = data->player.px_pos + cos(data->player.routation_ang) * data->player.movestep + 6;
 	y = data->player.py_pos + sin(data->player.routation_ang) * data->player.movestep + 6;
     if(data->map[y / CARE][x / CARE] != '1')
-    {
-        data->player.walk_dir = 1; 
         update_walk(data);
-    }
-   
-    // }
     return (0);
 }
 
@@ -35,12 +31,12 @@ int move_down(t_data *data)
     int x;
     int y;
     
+    data->player.walk_dir = -1;
     data->player.movestep = data->player.walk_dir * MOVE_SPEED;
 	x = data->player.px_pos + cos(data->player.routation_ang) * data->player.movestep + 6;
 	y = data->player.py_pos  + sin(data->player.routation_ang) * data->player.movestep + 6;
     if(data->map[y / CARE][x / CARE] != '1')
     {
-        data->player.walk_dir = -1;
         data->player.py_pos += 1;
         update_walk(data);
     }
