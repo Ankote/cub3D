8c2b0b2cd62ec67a00f43ddbe8942e5ae94802da
Ayoub Ankote <aankote@e1r2p1.1337.ma>
2023-05-20 12:31:15 +0100

    UPDATED

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: aankote <aankote@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 10:39:40 by aankote           #+#    #+#             */
-/*   Updated: 2023/05/20 11:30:17 by aankote          ###   ########.fr       */
+/*   Updated: 2023/05/20 11:55:28 by aankote          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int main(int ac ,char **av)
 	data->map = split_map(fd);
 	create_window(&window, data);
 	get_dimensions(data); 
-	data->player.routation_ang =  PI;
+	data->player.routation_ang = PI;
 	data->player.ray_angle = data->player.routation_ang - rad(30);
 	data->player.turn_dir = 0;
 	data->player.walk_dir = 0;
--- mini_map/check_ver.c
+++ mini_map/check_ver.c
@@ -6,7 +6,7 @@
 /*   By: aankote <aankote@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 10:42:10 by aankote           #+#    #+#             */
-/*   Updated: 2023/05/20 11:31:05 by aankote          ###   ########.fr       */
+/*   Updated: 2023/05/20 11:50:08 by aankote          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ void get_intercepts_ver(t_data *data)
     
     data->cords.xinterc_ver = floor(data->player.px_pos / CARE) * CARE;
     check_left = 0;
-    if(cos(data->player.routation_ang <= 1))
+    if(cos(data->player.routation_ang <= 0))
         check_left = 1;
     if(check_left)
-         data->cords.xinterc_ver += CARE;
-    data->cords.xinterc_ver = data->player.py_pos + tan(data->player.routation_ang) * CARE;
+         data->cords.xinterc_ver -= CARE;
+    data->cords.yinterc_ver = data->player.py_pos + tan(data->player.routation_ang) * (+data->cords.xinterc_ver + data->player.px_pos);
     draw_ray(data, data->cords.xinterc_ver,  data->cords.yinterc_ver, GREEN);                                                                                                                          
 }
