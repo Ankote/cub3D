944e51ff34c04b230bca96255ad81681b3dcf3ce
Ayoub Ankote <aankote@e1r2p1.1337.ma>
2023-05-20 17:31:25 +0100

    UPDATED

--- mini_map/check_ver.c
+++ mini_map/check_ver.c
@@ -6,7 +6,7 @@
 /*   By: aankote <aankote@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 10:42:10 by aankote           #+#    #+#             */
-/*   Updated: 2023/05/20 16:18:39 by aankote          ###   ########.fr       */
+/*   Updated: 2023/05/20 17:01:35 by aankote          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,23 @@ void get_intercepts_ver(t_data *data)
 {
 
 
-    int check_left;
+    int look_right;
     
     data->cords.xinterc_ver = floor(data->player.px_pos / CARE) * CARE;
-    check_left = 0;
-    if(cos(data->player.routation_ang <= 0))
-        check_left = 1;
-    if(!check_left)
+    look_right = 0;
+    if(cos(data->player.routation_ang >= 0))
+        look_right = 1;
+    if(look_right)
     {
         data->cords.xinterc_ver += CARE;
-        data->cords.yinterc_ver = data->player.py_pos + fabs(tan(data->player.routation_ang) * (data->cords.xinterc_ver - data->player.px_pos));
+        data->cords.yinterc_ver = data->player.py_pos + tan(data->player.routation_ang) * (data->cords.xinterc_ver - data->player.px_pos);
     }
     else
+    {
+        printf("fuck2\n");
         data->cords.yinterc_ver = data->player.py_pos + fabs(tan(data->player.routation_ang) * (data->cords.xinterc_ver - data->player.px_pos));
+    }
+        
     draw_ray(data, data->cords.xinterc_ver,  data->cords.yinterc_ver, GREEN);                                                                                                                          
 }
 
